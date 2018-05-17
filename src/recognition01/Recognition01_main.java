9da4d6c18ea9341d62578e1a998483e113c0a873
j16009 <35548197+j16009@users.noreply.github.com>
2018-05-17 12:32:18 +0900

    First Commit

---
+++ src/recognition01/Recognition01_main.java
@@ -0,0 +1,29 @@
+package recognition01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
+public class Recognition01_main {
+
+	public static void main(String[] args){
+
+	VisualRecognition service = new VisualRecognition("2018-03-19");
+	service.setApiKey("J16009");
+
+	DetectFacesOptions detectFacesOptions = null;
+	try {
+		detectFacesOptions = new DetectFacesOptions.Builder()
+		  .imagesFile(new File("img/tbn.png"))
+		  .build();
+	} catch (FileNotFoundException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	}
+	DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
+	System.out.println(result);
+	}
+}
