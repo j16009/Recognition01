001f9eb8d7c8ff627a26f7942967cbb7f4c96bcf
j16009 <35548197+j16009@users.noreply.github.com>
2018-05-24 12:47:01 +0900

    second commit

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,7 +2,10 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -11,19 +14,45 @@ public class Recognition01_main {
 
 	public static void main(String[] args){
 
-	VisualRecognition service = new VisualRecognition("2018-03-19");
-	service.setApiKey("J16009");
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		service.setApiKey("J16009");
 
-	DetectFacesOptions detectFacesOptions = null;
-	try {
-		detectFacesOptions = new DetectFacesOptions.Builder()
-		  .imagesFile(new File("img/tbn.png"))
-		  .build();
-	} catch (FileNotFoundException e) {
-		// TODO 自動生成された catch ブロック
-		e.printStackTrace();
-	}
-	DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
-	System.out.println(result);
+		DetectFacesOptions detectFacesOptions = null;
+		try {
+			detectFacesOptions = new DetectFacesOptions.Builder()
+					.imagesFile(new File("img/tbn.png"))
+					.build();
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
+		System.out.println(result);
+		String s = String.valueOf(result);
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			JsonNode node = mapper.readTree(s);
+
+			int age_min = node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+			System.out.println("age_min : " + age_min);
+
+			int age_max = node.get("images").get(0).get("faces").get(0).get("age").get("max").asInt();
+			System.out.println("age_max : " + age_max);
+
+			float age_score = node.get("images").get(0).get("faces").get(0).get("age").get("score").floatValue();
+			System.out.println("age_score : " + age_score);
+
+			String Gender = node.get("images").get(0).get("faces").get(0).get("gender").get("gender").asText();
+			System.out.println("Gender : " + Gender);
+
+			float Gender_score = node.get("images").get(0).get("faces").get(0).get("gender").get("score").floatValue();
+			System.out.println("Gender_score : " + Gender_score);
+
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
 	}
 }
